f47d980f2d3ef4d8e64bfb6e03db763a31aa5be8
Guillaume Maiano <guillaume@maiano.fr>
2019-08-26 13:54:26 +0200

    Nice autoclosure technique by Sundell that does type inference on the fly on untyped dictionaries' data extraction

--- AutoclosureErrorManagement.playground/Contents.swift
+++ AutoclosureErrorManagement.playground/Contents.swift
@@ -20,3 +20,19 @@ extension Optional {
 // only evaluate error expression when needed
 // TODO: code an example that does compile
 // let name = try argument(at: 1).unwrapOrThrow(ArgumentError.missingName)
+
+/// Cool technique for extracting optional data
+/// from Dictionary, db, userdefaults....
+// instead of let coins = (dictionary["numberOfCoins"] as? Int) ?? 100
+// use a more readable form:
+//
+// let coins = dictionary.value(forKey: "numberOfCoins", defautValue: 100)
+// via an extension with autoclosures:
+extension Dictionary where Value == Any {
+    func value<T>(forKey key: Key, defaultValue: @autoclosure () -> T) -> T {
+        guard let value = self[key] as? T else {
+            return defaultValue()
+        }
+    return value
+    }
+}
